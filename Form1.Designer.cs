62856184a64f11b29dfd52df0dae29400c8d10c9
Nadya Rakmeni <142906337+20NadyaRakmeni@users.noreply.github.com>
2024-09-23 19:28:54 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WinFormsApp6_framework_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            textUsername = new TextBox();
+            textPassword = new TextBox();
+            btnLogin = new Button();
+            btnCancel = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.download;
+            pictureBox1.Location = new Point(12, 24);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(219, 234);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(265, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Username";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(265, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Password";
+            label2.Click += label2_Click;
+            // 
+            // textUsername
+            // 
+            textUsername.Location = new Point(348, 33);
+            textUsername.Multiline = true;
+            textUsername.Name = "textUsername";
+            textUsername.Size = new Size(141, 23);
+            textUsername.TabIndex = 3;
+            textUsername.TextChanged += textBox1_TextChanged;
+            // 
+            // textPassword
+            // 
+            textPassword.Location = new Point(348, 84);
+            textPassword.Multiline = true;
+            textPassword.Name = "textPassword";
+            textPassword.PasswordChar = '*';
+            textPassword.Size = new Size(141, 23);
+            textPassword.TabIndex = 4;
+            textPassword.TextChanged += textBox2_TextChanged;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(642, 278);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(513, 278);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 6;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancel);
+            Controls.Add(btnLogin);
+            Controls.Add(textPassword);
+            Controls.Add(textUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textUsername;
+        private TextBox textPassword;
+        private Button btnLogin;
+        private Button btnCancel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WinFormsApp6_framework_
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+
+        private DataSet ds = new DataSet();
+        private string alamat, query;
+        public Form1()
+        {
+            alamat = "server=localhost; database=db_RumahSakit; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                query = string.Format("select * from tbl_pengguna where username = '{0}'", textUsername.Text);
+                ds.Clear();
+                koneksi.Open();
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                adapter.Fill(ds);
+                koneksi.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow kolom in ds.Tables[0].Rows)
+                    {
+                        string sandi;
+                        sandi = kolom["Password"].ToString();
+                        if (sandi == textPassword.Text)
+                        {
+                            frmMain FrmMain = new frmMain();
+                            FrmMain.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Anda Salah Menginput Username atau Password");
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Username tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
